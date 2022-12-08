21bad3583b2f58cab6cbdaf3b118c3d1e4af6191
Tyson Tang <90588335+tysonking33@users.noreply.github.com>
2022-12-08 07:11:54 +0000

    edited delete at data

--- linked_lists/linked_list.cpp
+++ linked_lists/linked_list.cpp
@@ -1,7 +1,8 @@
 #include "Linked_list.h"
 #include <iostream>
 
-int Linked_List::util_len(Node** head_ref){
+int Linked_List::util_len(Node **head_ref)
+{
     Node *new_node = new Node();
     new_node = head_ref;
     int len;
@@ -12,7 +13,8 @@ int Linked_List::util_len(Node** head_ref){
     }
     return len;
 }
-void Linked_List::util_print(Node** head_ref){
+void Linked_List::util_print(Node **head_ref)
+{
     Node *new_node = new Node();
     new_node = head_ref;
     while (new_node->next)
@@ -43,7 +45,8 @@ void Linked_List::insertAfter(Node *prev_node, int new_data)
     prev_node->next = new_node;
 }
 
-void Linked_List::append(Node** head_ref, int new_data){
+void Linked_List::append(Node **head_ref, int new_data)
+{
     Node *new_node = new Node();
     new_node->data = new_data;
     new_node->next = NULL;
@@ -55,15 +58,16 @@ void Linked_List::append(Node** head_ref, int new_data){
     last->next = new_node;
 }
 
-void Linked_List::deleteFront(Node** head_ref){
+void Linked_List::deleteFront(Node **head_ref)
+{
     Node *new_head = new Node();
     new_head = (*head_ref);
     (*head_ref) = new_head->next;
-    delete(new_head);
-
+    delete (new_head);
 }
 
-void Linked_List::deleteEnd(Node** head_ref){
+void Linked_List::deleteEnd(Node **head_ref)
+{
     Node *to_delete = new Node();
     Node *new_end = new Node();
     new_end = (*head_ref);
@@ -72,11 +76,12 @@ void Linked_List::deleteEnd(Node** head_ref){
         new_end = new_end->next;
     }
     to_delete = new_end;
-    delete(to_delete);
+    delete (to_delete);
     new_end->next = NULL;
 }
 
-void Linked_List::deletePos(Node** head_ref, int pos){
+void Linked_List::deletePos(Node **head_ref, int pos)
+{
     Node *temp1 = new Node();
     temp1 = (*head_ref);
     Node *new_mid = new Node();
@@ -91,29 +96,23 @@ void Linked_List::deletePos(Node** head_ref, int pos){
     }
     new_mid = temp1;
     new_mid->next = temp1->next;
-    delete(temp1);
+    delete (temp1);
 }
 
-void deleteAtData(Node** head_ref, int key){
+void deleteAtData(Node **head_ref, int key)
+{
     Node *temp1 = new Node();
     temp1 = (*head_ref);
     Node *new_mid = new Node();
-    if (pos > util_len(head_ref))
-    {
-        std::cout << "error pos larger than list length";
-    }
     while (temp1->data != key)
     {
         temp1 = temp1->next;
-    }s
+    }
     new_mid = temp1;
     new_mid->next = temp1->next;
-    delete(temp1);
+    delete (temp1);
 }
 
-void swapAB(Node** head_ref, int A, int B){
+void swapAB(Node **head_ref, int A, int B)
+{
 }
-
-
-
-
